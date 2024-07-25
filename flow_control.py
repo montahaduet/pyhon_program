9e9379ccf7909a8b94087ab1abc5511300b6ff3f
montaha <esorwar.cse@gmail.com>
2024-07-25 09:49:24 +0600

    Add flow controll

---
+++ flow_control.py
@@ -0,0 +1,42 @@
+#if-else statement
+"""
+if 2<1:
+    print("2 is large")
+elif 1<0:
+    print("2 is smalar")
+else:
+    print("another number")
+
+"""
+# if else----------------------------------- 
+"""
+print("enter command")
+robot_move=input()
+if robot_move=='front':
+    print('moving front')
+elif robot_move=='back':
+    print('moving back')  
+else:
+    print("still now")  
+"""
+#while loop-------------------------------------
+
+# x=0
+# while(x<10):
+#     print(x)
+#     x=x+1
+
+#------
+
+
+########for loo-----------------------------
+# for i in range(1,10):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+    
+
+
+
