ef74f626aef960dae3ccdd535ba2ea756ee5d5e9
montaha <montaha.cmt@gmail.com>
2024-07-26 08:30:47 +0600

    user name print

--- function.py
+++ function.py
@@ -1,5 +1,11 @@
-def add(a,b):
-    return a+b
-sum=int(add(4,6))
-print("sum is="+str(sum))
+# def add(a,b):
+#     return a+b
+# sum=int(add(4,6))
+# print("sum is="+str(sum))
 
+#_____________________________
+def username(name):
+    print("my name is :"+name)
+myname=input()
+username(myname)
+ 
