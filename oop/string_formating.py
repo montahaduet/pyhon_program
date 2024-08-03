60ff24041af5e2775600b18ad4b7e60a68cca33c
montaha <montaha.cmt@gmail.com>
2024-08-03 20:52:15 +0600

    string formating

---
+++ oop/string_formating.py
@@ -0,0 +1,14 @@
+print('i am %s' % 'mun')
+print('i am %s and i am %d years old'% ('muntaha',23))
+name='mark'
+age=25
+profession='player'
+earning=34000.45
+
+print('i am %s and i am %d years old.i am a %s and i earn %.2f$'%(name,age,profession,earning))
+
+print('i am {} and i am {}'.format('akil','happy'))
+print('i am {1} and i am {0}'.format(name,'happy'))
+food='sandwich'
+language='python'
+print(f'i am {name} and i eat {food}.i don\'t believe in {language} haters\n and {2**2}=={4}')
